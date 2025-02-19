78cfc5164a44d5e9a7f8e0f59ac5afd9a16b1221
Zayed <zayedkh02@gmail.com>
2025-02-19 12:51:19 +0000

    Created useWordle hook and setup function templates

---
+++ src/hooks/useWordle.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+const useWordle = (solution) => {
+    // format a guess into an array of letter objects
+    // e.g {key: 'a', 'color': yellow}
+
+    const formatGuess = () => {
+
+    }
+
+    // add a new guess to the guesses state
+    // update the isCorrect state if the guess is correct
+    // add one to the turn state
+    const addNewGuess = () => {
+        const [guesses, setGuesses] = useState();
+    }
+
+    // handle keyup event & track current guess
+    // if user presses enter, add the new guess
+    
+    const handleKeyUp = () => {
+        
+    }
+
+}
+
+export default useWordle
