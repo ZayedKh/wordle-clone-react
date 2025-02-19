fb1e3082abf843e1570064e87f50cba5028b2392
Zayed <zayedkh02@gmail.com>
2025-02-19 22:28:28 +0000

    Added row functionality, added styles for each box in grid

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -2,25 +2,25 @@ import React, { useEffect } from 'react'
 import Grid from './Grid'
 import useWordle from '../hooks/useWordle'
 
-export default function Wordle({solution}) {
+export default function Wordle({ solution }) {
 
     const { currentGuess, guesses, handleKeyUp, isCorrect, turn } = useWordle(solution);
 
     useEffect(() => {
         window.addEventListener('keyup', handleKeyUp);
 
-        return () => window.removeEventListener('keyup',handleKeyUp);
+        return () => window.removeEventListener('keyup', handleKeyUp);
     }, [handleKeyUp]);
 
     useEffect(() => {
-        console.log(guesses, isCorrect, turn); 
+        console.log(guesses, isCorrect, turn);
     }, [guesses, turn, isCorrect]);
 
     return (
         <>
-        <div>Solution: {solution}</div>
-        <div>Current guess: {currentGuess}</div>
-        <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
+            <div>Solution: {solution}</div>
+            <div>Current guess: {currentGuess}</div>
+            <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
         </>
     )
 }
