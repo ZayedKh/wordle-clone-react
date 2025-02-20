9915173c8f03ac4853d8bfe402d05590b6cf2455
Zayed <zayedkh02@gmail.com>
2025-02-20 00:00:59 +0000

    Added letters showing in grid as user types

--- src/components/Grid.js
+++ src/components/Grid.js
@@ -6,6 +6,9 @@ export default function Grid({ currentGuess, guesses, turn }) {
     return (
         <>
             {guesses.map((guess, i) => {
+                if(turn === i){
+                    return <Row key={i} currentGuess={currentGuess}/>
+                }
                 return <Row key={i} guess={guess}></Row>
             })}
         </>
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,16 +1,32 @@
 import React from 'react'
 
-export default function Row({ guess }) {
+export default function Row({ guess, currentGuess }) {
 
 
-    if(guess){
-        return(
+    if (guess) {
+        return (
             <div className='row past'>
-                {guess.map((c, i) =>(
-                    <div key={i} className={c.color}>{c.key}</div>    
+                {guess.map((c, i) => (
+                    <div key={i} className={c.color}>{c.key}</div>
                 ))}
             </div>
         )
+    }
+
+    if (currentGuess) {
+
+        let letters = currentGuess.split('');
+
+        return (
+            <div className='row current'>
+                {letters.map((c, i) => (
+                    <div key={i} className='filled'>{c}</div>
+                ))}
+                {[...Array(5 - letters.length)].map((_,i) => (
+                    <div key={i}></div>
+                ))}
+            </div>
+        );
     }
 
     return (
